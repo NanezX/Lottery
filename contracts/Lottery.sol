e4f54d1a124620fc25696fb083f849e6f74e26d8
NanezX <victor.nanezj@gmail.com>
2021-06-06 13:48:46 -0400

    Adding owneables upgradeable Oz

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "./interfaces/CERC20.sol";
 import "hardhat/console.sol";
 
-contract Lottery is Initializable {
+contract Lottery is OwnableUpgradeable {
     uint fee;
     function initialize(uint256 _fee) public initializer {
         fee = _fee;
